c9c9c1925c4cc97155be5628894390f014917f82
Mark Piper <mark.piper@colorado.edu>
2025-07-21 18:10:29 -0600

    Write C model server in C++

--- server/heatc-grpc4bmi-server.cxx
+++ server/heatc-grpc4bmi-server.cxx
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 #include "bmi_heat.h"
 #include "bmi_grpc_server.h"
@@ -6,9 +6,19 @@
 
 int main(int argc, char *argv[])
 {
+    printf("BmiHeat C grpc4bmi server\n");
+
     Bmi *model = (Bmi *) malloc(sizeof(Bmi));
 
     register_bmi_heat(model);
+
+    {
+        char model_name[BMI_MAX_COMPONENT_NAME];
+        
+        model->get_component_name(model, model_name);
+        printf("%s\n", model_name);
+    }
+
     run_bmi_server(model, argc, argv);
 
     free(model);
